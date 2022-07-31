d5eb11086dfc888c0e7eeeb15e826d256d85cb22
jmpnz <dumpsterdev@gmail.com>
2022-07-31 21:54:15 +0100

    fix:add file_manager.cpp to CMake build

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,8 @@
 #include <iostream>
 
-int main() { std::cout << "Hello, caskdb" << std::endl; }
+#include "file_manager.hpp"
+
+int main() {
+  auto fm = caskdb::FileManager("cask.db");
+  std::cout << "Hello, caskdb " << fm.FileName() << std::endl;
+}
